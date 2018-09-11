9793d6576f40fe8e0ba1749021ff176070c4b83b
Nate Davis <orpheusdevelopment@gmail.com>
2018-09-11 17:04:30 -0500

    General Text Field

--- src/stories/input.js
+++ src/stories/input.js
@@ -7,8 +7,9 @@ import styles from "@sambego/storybook-styles";
 
 //Search
 import Search from '../components/input/search/ButtonSearch.js'
-//Password
+//Text Fields
 import PasswordField from '../components/input/fields/password/PasswordField.js'
+import GeneralField from '../components/input/fields/general/GeneralField.js'
 
 
 
@@ -22,4 +23,5 @@ storiesOf('Input', module)
       }))
     //List out different inputs
     .add('Search', () => ( <Search />))
-    .add('Password Field', () => ( <PasswordField />));
+    .add('Password Field', () => ( <PasswordField />))
+    .add('General Field', () => ( <GeneralField />));
